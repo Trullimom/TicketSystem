832a675f70e8035e53dcd9a8f4f575ccccef9fd5
Trullimom <112940668+Trullimom@users.noreply.github.com>
2025-01-23 12:29:42 +0100

    update

--- TicketSystem/Models/Anfrage.cs
+++ TicketSystem/Models/Anfrage.cs
@@ -11,6 +11,8 @@
         public string Telefon { get; set; }
         public string Nachricht { get; set; }
 
+        public string MitarbeiterName { get; set; }
+
         public DateTime Datum { get; set; }
 
         public bool Erledigt { get; set; }
--- TicketSystem/Models/AnfragenListe.cs
+++ TicketSystem/Models/AnfragenListe.cs
@@ -2,7 +2,18 @@
 {
     public class AnfragenListe
     {
-        public static List<Anfrage> anfragenListe { get; set; } = new List<Anfrage>();
+        public static List<Anfrage> anfragenListe { get; set; } = new List<Anfrage>()
+        {
+            new Anfrage{ KundenName= "Kunde1", MitarbeiterName = "Andre", DeadLine =new DateTime(2025,01,23), Erledigt= false, Kommentar= "blablabla" },
+            new Anfrage{ KundenName= "Kunde2", MitarbeiterName = "Jihye", DeadLine =new DateTime(2025,01,23), Erledigt= true, Kommentar= "blablabla" },
+            new Anfrage{ KundenName= "Kunde3", MitarbeiterName = "Marcel", DeadLine =new DateTime(2025,01,23), Erledigt= false, Kommentar= "blablabla" },
+            new Anfrage{ KundenName= "Kunde4", MitarbeiterName = "Abdullah", DeadLine =new DateTime(2025,01,23), Erledigt= false, Kommentar= "blablabla" },
+
+
+        };
+
+
+
 
     }
 }
