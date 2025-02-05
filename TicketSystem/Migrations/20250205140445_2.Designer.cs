66dc7673464bda82008ddf55a0833a5387f46ddd
Trullimom <112940668+Trullimom@users.noreply.github.com>
2025-02-05 15:46:43 +0100

    Final

--- TicketSystem/Migrations/20250205140445_2.Designer.cs
+++ TicketSystem/Migrations/20250205140445_2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketSystem.Models.Data;
 
@@ -11,9 +12,11 @@ using TicketSystem.Models.Data;
 namespace TicketSystem.Migrations
 {
     [DbContext(typeof(AnwendungsDbContext))]
-    partial class AnwendungsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250205140445_2")]
+    partial class _2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -46,10 +49,6 @@ namespace TicketSystem.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("Kommentar")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("KommentarUser")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime?>("KommentarZeit")
